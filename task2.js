0d430fff91e5d075e34d5e82e54b2976aba8d505
mind <mind@minds-Mac-mini.local>
2024-11-29 19:14:36 +0530

    feat: calc

--- task2.js
+++ task2.js
@@ -1,78 +1,85 @@
-// All records have unique id. status signifies if that record is ready/available or not.
-// There are dependencies among the records. Like => id=4 is dependent on id=3 and id=1, id=3 is having further dependencies.
-// A record's name can be fetched if it's status and all it's dependency's are having status=1.
-// Also check there should be no circular dependencies like => id=1 is dependent on id=2 and id=2 is dependent on 1
-
+// task: Calculate days between two dates without using any Date functions in JS
 const express = require("express");
 const app = express();
-const array = [
-  {
-    id: 1,
-    name: "de",
-    // dependecies: [4],
-    status: 1,
-  },
-  {
-    id: 2,
-    name: "fv",
-    dependecies: [1],
-    status: 1,
-  },
-  {
-    id: 3,
-    name: "werf",
-    dependecies: [1, 2],
-    status: 1,
-  },
-  {
-    id: 4,
-    name: "werf",
-    dependecies: [3],
-    status: 1,
-  },
-];
-
-const dataMap = new Map(array.map((item) => [item.id, item]));
-const Ids = {};
-function fetchRecord(id) {
-  if (Ids[id]) {
-    return { code: Infinity };
+const validateDate = (date) => {
+  const dateSplit = date.split("/");
+  if (dateSplit?.length > 3 && dateSplit?.length < 3) {
+    return `Invalid Date`;
   }
-  Ids[id] = true;
-
-  const data = dataMap.get(id);
-  if (!data || data.status !== 1) {
-    delete Ids[id];
-    return { message: "record is not ready / unavailable", code: 0 };
-  }
-
-  const check = data?.dependecies?.map((depId) => fetchRecord(depId));
-  if (check?.length) {
-    const includeCodeInfinity = check.some(
-      (record) => record.code === Infinity
-    );
-    if (includeCodeInfinity) {
-      delete Ids[id];
-      return { message: "circular dependency found", code: Infinity };
+  if (dateSplit[2] >= 1900 && dateSplit[2] <= 9999) {
+    if (dateSplit[1] >= 1 && dateSplit[1] <= 12) {
+      if (
+        dateSplit[0] >= 1 &&
+        dateSplit[0] <= 31 &&
+        (dateSplit[1] == 1 ||
+          dateSplit[1] == 3 ||
+          dateSplit[1] == 5 ||
+          dateSplit[1] == 7 ||
+          dateSplit[1] == 8 ||
+          dateSplit[1] == 10 ||
+          dateSplit[1] == 12)
+      )
+        return { dateSplit, code: 1 };
+      else if (
+        dateSplit[0] >= 1 &&
+        dateSplit[0] <= 30 &&
+        (dateSplit[1] == 4 ||
+          dateSplit[1] == 6 ||
+          dateSplit[1] == 9 ||
+          dateSplit[1] == 11)
+      )
+        return { dateSplit, code: 1 };
+      else if (dateSplit[0] >= 1 && dateSplit[0] <= 28 && dateSplit[1] == 2)
+        return { dateSplit, code: 1 };
+      else if (
+        dateSplit[0] == 29 &&
+        dateSplit[1] == 2 &&
+        (dateSplit[2] % 400 == 0 ||
+          (dateSplit[2] % 4 == 0 && dateSplit[2] % 100 != 0))
+      )
+        return { dateSplit, code: 1 };
+      else return "Day is invalid.\n";
+    } else {
+      return "Month is not valid.\n";
     }
-    const includeCodeZero = check.some((record) => record.code === 0);
-    if (includeCodeZero) {
-      delete Ids[id];
-      return {
-        message: "record's related dependency is not ready / unavailable",
-        code: 0,
-      };
-    }
+  } else {
+    return "Year is not valid.\n";
   }
+};
 
-  delete Ids[id];
-  return { message: "fetched successfully", data, code: 1 };
-}
+const countDays = (...arguments) => {
+  const [Date1, Date2] = Array.from(arguments).map((date) =>
+    validateDate(date)
+  );
+  if (Date1.code !== 1 || Date2.code !== 1) {
+    return `Invalid Dates`;
+  }
+  console.log(Date1.dateSplit);
+  let difDays;
+  let difMonth;
+  let difYear;
 
-console.log(fetchRecord(4));
+  difDays = parseInt(Date1.dateSplit[0]) - parseInt(Date2.dateSplit[0]);
+  difMonth = parseInt(Date1.dateSplit[1]) - parseInt(Date2.dateSplit[1]);
+  difYear = parseInt(Date1.dateSplit[2]) - parseInt(Date2.dateSplit[2]);
 
-//------------------------------------
-
+  console.log(difDays, difMonth, difYear);
+};
+console.log(countDays("11/05/2024", "20/01/2024"));
 app.listen(3003, () => {
-  console.log("server is running on 3003");
+  return "server is running on 3003";
 });
+
+// ------------- notes ---------
+// date is valid  =>  date has (valid days + valid months)
+
+//   // Calculate the difference in days
+//   const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+//   const diffDays = Math.round(Math.abs((Date1 - Date2) / oneDay));
+
+//   return diffDays;
+// };
+
+// // Example usage
+// const days = countDays("2023-10-01", "2023-10-10");
+// console.log(days); // Output should be 9
